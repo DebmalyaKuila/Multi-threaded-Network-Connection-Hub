3e7d26f5704ca636f9757fb74411729e5a9f505e
Ghost <debmalyakuila2002@gmail.com>
2024-11-13 22:02:09 +0530

    code refactoring and reject clients more than MAX_CLIENTS

--- server/serverSocket.cpp
+++ server/serverSocket.cpp
@@ -7,6 +7,8 @@
 #include<pthread.h>
 using namespace std;
 
+#define MAX_CLIENTS 10
+
 int port=2000;
 
 struct acceptedSocket{
@@ -15,14 +17,14 @@ struct acceptedSocket{
     int error;
     int acceptedSuccesfully;
 };
-void recvAndPrintIncomingChatOnSeprateThread(int clientSocketFD);
+void recvAndPrintIncomingChatOnSeprateThread(int i);
 struct acceptedSocket* acceptIncomingConnection(int serverSocketFD);
-void *acceptNewConnectionAndRecvAndPrintChat(void* serverSockFDAddr);
-void recvAndPrintIncomingChat(int clientSocketFD);
-void sendRecvMsgToOtherClients(int incomingMsgclientSocketFD,char*buffer);
+void recvAndPrintIncomingChat(int clientSocketIndex);
+void broadcastClients(int incomingMsgclientSocketFD,char*buffer);
 
-struct acceptedSocket clientSockets[10];
+struct acceptedSocket* clientSockets[MAX_CLIENTS];
 int clientSocketCount=0;
+
 
 int main(){
 
@@ -45,71 +47,85 @@ int main(){
 
     //start accepting incoming connections
     while(true){
-    struct acceptedSocket* clientSocket=acceptIncomingConnection(serverSocketFD);
-    clientSockets[clientSocketCount++]=*clientSocket;
-    recvAndPrintIncomingChatOnSeprateThread(clientSocket->acceptedSocketFD);
+        struct acceptedSocket* clientSocket=acceptIncomingConnection(serverSocketFD);
+        if(clientSocket==NULL)continue;
+        for(int i=0;i<MAX_CLIENTS;i++){
+            if(clientSockets[i]==NULL){
+                clientSockets[i]=clientSocket;
+                clientSocketCount++;
+                cout<<"Number of live connections : "<<clientSocketCount<<endl;
+                recvAndPrintIncomingChatOnSeprateThread(i);
+                break;
+            }
+        }
     }
     
     shutdown(serverSocketFD,SHUT_RDWR);
     return 0;
 }
 
+
 struct acceptedSocket* acceptIncomingConnection(int serverSocketFD){
     struct sockaddr_in clientAddr;
     int clientAddrSize=sizeof(struct sockaddr_in);
     int clientSocketFD=accept(serverSocketFD,(struct sockaddr*)&clientAddr,(socklen_t *)&clientAddrSize);
-     if (clientSocketFD < 0) {
-        // Handle error in accepting connection
+    if (clientSocketFD < 0) {
+        perror("Client accept failed");
         return NULL;
     }
+
+    if (clientSocketCount >= MAX_CLIENTS) {
+        // Reject the client if max clients reached
+        cout << "Rejecting client: Maximum clients reached." << endl;
+        close(clientSocketFD); // Close the socket immediately
+        return NULL;
+    }
+
     struct acceptedSocket* acceptedSock = (struct acceptedSocket*)malloc(sizeof(struct acceptedSocket));
     acceptedSock->acceptedSocketFD=clientSocketFD;
     acceptedSock->acceptedSuccesfully=(clientSocketFD>0);
     if(!(acceptedSock->acceptedSuccesfully)) acceptedSock->error=clientSocketFD;
-    // Add the client to the list of active clients
     cout << "New client connected: " << clientSocketFD << endl;
     return acceptedSock;
 }
 
-void *acceptNewConnectionAndRecvAndPrintChat(void* serverSockFDAddr){
-     int serverSocketFD = *((int*)serverSockFDAddr);
-     while(true){
-     struct acceptedSocket* clientSocket=acceptIncomingConnection(serverSocketFD);
-     recvAndPrintIncomingChatOnSeprateThread(clientSocket->acceptedSocketFD);
-     }
-
-    return NULL;
-}
 
 
-
-void *recvAndPrintIncomingChat(void* clientSockFDAddr){
-    int clientSocketFD = *((int*)clientSockFDAddr);
+void *recvAndPrintIncomingChat(void* clientSocketIndex){ 
+    int pos = *(int*)clientSocketIndex;
+    struct acceptedSocket* clientSocket =clientSockets[pos];
     char buff[1024];
     while(true){
-        int bytesRecv=recv(clientSocketFD,buff,1024,0);
+        int bytesRecv=recv(clientSocket->acceptedSocketFD,buff,1024,0);
         if(bytesRecv>0){
             buff[bytesRecv]='\0';
             cout<<buff<<endl;
             //broadcasting incming message to all clients 
-            sendRecvMsgToOtherClients(clientSocketFD,buff);
+            broadcastClients(clientSocket->acceptedSocketFD,buff);
         }
-        if(bytesRecv==0)break;
+        if(bytesRecv==0){
+            cout<<"client "<<(clientSocket->acceptedSocketFD)<<" closed connection"<<endl;
+            break;
+        }
     }
-    close(clientSocketFD);
+    close(clientSocket->acceptedSocketFD);
+    free(clientSocket);
+    clientSockets[pos]=NULL;
+    clientSocketCount--;
+    cout<<"Number of live connections : "<<clientSocketCount<<endl;
 
     return NULL;
 }
 
-void recvAndPrintIncomingChatOnSeprateThread(int clientSocketFD){
+void recvAndPrintIncomingChatOnSeprateThread(int i){
     pthread_t id;
-    pthread_create(&id,NULL,recvAndPrintIncomingChat,(void*)&clientSocketFD);
+    pthread_create(&id,NULL,recvAndPrintIncomingChat,(void*)&i);
 }
 
-void sendRecvMsgToOtherClients(int incomingMsgclientSocketFD,char* buffer){
-    for(int i=0;i<clientSocketCount;i++){
-        if(clientSockets[i].acceptedSocketFD!=incomingMsgclientSocketFD){
-            send(clientSockets[i].acceptedSocketFD,buffer,strlen(buffer),0);
+void broadcastClients(int incomingMsgClientSocketFD,char* buffer){
+    for(int i=0;i<MAX_CLIENTS;i++){
+        if(clientSockets[i]!=NULL && clientSockets[i]->acceptedSocketFD  != incomingMsgClientSocketFD ){
+            send(clientSockets[i]->acceptedSocketFD,buffer,strlen(buffer),0);
         }
     }
 }
